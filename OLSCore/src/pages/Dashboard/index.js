22a26485e836eeb03aaf88068fd13f72653e8074
binkyo1116 <topdevprince@gmail.com>
2022-07-15 21:05:38 +0300

    chart

--- OLSCore/src/pages/Dashboard/index.js
+++ OLSCore/src/pages/Dashboard/index.js
@@ -13,11 +13,320 @@ import user2 from '../../assets/images/user-2.png'
 import user3 from '../../assets/images/user-3.png'
 import user4 from '../../assets/images/user-4.png'
 import { ProgressBar, Popover, OverlayTrigger,ListGroup,ListGroupItem } from 'react-bootstrap'
+import { VictoryBar, VictoryChart, VictoryAxis, TextSize, Bar, VictoryLabel, VictoryTheme } from 'victory';
+import { styled } from '@mui/system'
+
+const data = [
+  {quarter: 1, earnings: 100},
+  {quarter: 2, earnings: 100},
+  {quarter: 3, earnings: 82},
+  {quarter: 4, earnings: 87},
+  {quarter: 5, earnings: 87},
+  {quarter: 6, earnings: 87},
+  {quarter: 7, earnings: 87},
+  {quarter: 8, earnings: 87},
+  {quarter: 9, earnings: 87},
+  {quarter: 10, earnings: 87},
+];
 
 const initialState = {
+  data: {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July"
+    ],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "#0069b4b3",
+        borderColor: "#0069b4",
+        borderWidth: 1,
+        hoverBackgroundColor: "#0069b447",
+        hoverBorderColor: "#0069b4",
+        data: [65, 59, 100, 81, 56, 55, 40]
+      }
+    ]
+  },
+};
+let quaters = [];
+let tickValues = [];
 
-  
+for (let i = 1; i < 11; i ++) {
+  quaters.push("Week" + i);
+  tickValues.push(i);
 }
+
+const StyledBar = styled(Bar)`
+  fill: purple;
+`
+const StyledLabel = styled(VictoryLabel)`
+  tspan {
+    fill: magenta;
+    font-family: Papyrus, fantasy;
+  }
+`
+
+// const colors = [
+//   "#252525",
+//   "#525252",
+//   "#737373",
+//   "#969696",
+//   "#bdbdbd",
+//   "#d9d9d9",
+//   "#f0f0f0"
+// ];
+// const charcoal = "#252525";
+// const grey = "#969696";
+
+// // Typography
+// const sansSerif = "'Gill Sans', 'Seravek', 'Trebuchet MS', sans-serif";
+// const letterSpacing = "normal";
+// const fontSize = 14;
+
+// // Layout
+// const baseProps = {
+//   width: 450,
+//   height: 300,
+//   padding: 50,
+//   colorScale: colors
+// };
+
+// // Labels
+// const baseLabelStyles = {
+//   fontFamily: sansSerif,
+//   fontSize,
+//   letterSpacing,
+//   padding: 10,
+//   fill: charcoal,
+//   stroke: "transparent"
+// };
+
+// const centeredLabelStyles = assign({ textAnchor: "middle" }, baseLabelStyles);
+
+// // Strokes
+// const strokeLinecap = "round";
+// const strokeLinejoin = "round";
+
+// // Put it all together...
+
+// const theme = {
+//   area: assign(
+//     {
+//       style: {
+//         data: {
+//           fill: charcoal
+//         },
+//         labels: baseLabelStyles
+//       }
+//     },
+//     baseProps
+//   ),
+//   axis: assign(
+//     {
+//       style: {
+//         axis: {
+//           fill: "transparent",
+//           stroke: charcoal,
+//           strokeWidth: 1,
+//           strokeLinecap,
+//           strokeLinejoin
+//         },
+//         axisLabel: assign({}, centeredLabelStyles, {
+//           padding: 25
+//         }),
+//         grid: {
+//           fill: "none",
+//           stroke: "none",
+//           pointerEvents: "painted"
+//         },
+//         ticks: {
+//           fill: "transparent",
+//           size: 1,
+//           stroke: "transparent"
+//         },
+//         tickLabels: baseLabelStyles
+//       }
+//     },
+//     baseProps
+//   ),
+//   bar: assign(
+//     {
+//       style: {
+//         data: {
+//           fill: charcoal,
+//           padding: 8,
+//           strokeWidth: 0
+//         },
+//         labels: baseLabelStyles
+//       }
+//     },
+//     baseProps
+//   ),
+//   boxplot: assign(
+//     {
+//       style: {
+//         max: { padding: 8, stroke: charcoal, strokeWidth: 1 },
+//         maxLabels: assign({}, baseLabelStyles, { padding: 3 }),
+//         median: { padding: 8, stroke: charcoal, strokeWidth: 1 },
+//         medianLabels: assign({}, baseLabelStyles, { padding: 3 }),
+//         min: { padding: 8, stroke: charcoal, strokeWidth: 1 },
+//         minLabels: assign({}, baseLabelStyles, { padding: 3 }),
+//         q1: { padding: 8, fill: grey },
+//         q1Labels: assign({}, baseLabelStyles, { padding: 3 }),
+//         q3: { padding: 8, fill: grey },
+//         q3Labels: assign({}, baseLabelStyles, { padding: 3 })
+//       },
+//       boxWidth: 20
+//     },
+//     baseProps
+//   ),
+//   candlestick: assign(
+//     {
+//       style: {
+//         data: {
+//           stroke: charcoal,
+//           strokeWidth: 1
+//         },
+//         labels: assign({}, baseLabelStyles, { padding: 5 })
+//       },
+//       candleColors: {
+//         positive: "#ffffff",
+//         negative: charcoal
+//       }
+//     },
+//     baseProps
+//   ),
+//   chart: baseProps,
+//   errorbar: assign(
+//     {
+//       borderWidth: 8,
+//       style: {
+//         data: {
+//           fill: "transparent",
+//           stroke: charcoal,
+//           strokeWidth: 2
+//         },
+//         labels: baseLabelStyles
+//       }
+//     },
+//     baseProps
+//   ),
+//   group: assign(
+//     {
+//       colorScale: colors
+//     },
+//     baseProps
+//   ),
+//   histogram: assign(
+//     {
+//       style: {
+//         data: {
+//           fill: grey,
+//           stroke: charcoal,
+//           strokeWidth: 2
+//         },
+//         labels: baseLabelStyles
+//       }
+//     },
+//     baseProps
+//   ),
+//   legend: {
+//     colorScale: colors,
+//     gutter: 10,
+//     orientation: "vertical",
+//     titleOrientation: "top",
+//     style: {
+//       data: {
+//         type: "circle"
+//       },
+//       labels: baseLabelStyles,
+//       title: assign({}, baseLabelStyles, { padding: 5 })
+//     }
+//   },
+//   line: assign(
+//     {
+//       style: {
+//         data: {
+//           fill: "transparent",
+//           stroke: charcoal,
+//           strokeWidth: 2
+//         },
+//         labels: baseLabelStyles
+//       }
+//     },
+//     baseProps
+//   ),
+//   pie: {
+//     style: {
+//       data: {
+//         padding: 10,
+//         stroke: "transparent",
+//         strokeWidth: 1
+//       },
+//       labels: assign({}, baseLabelStyles, { padding: 20 })
+//     },
+//     colorScale: colors,
+//     width: 400,
+//     height: 400,
+//     padding: 50
+//   },
+//   scatter: assign(
+//     {
+//       style: {
+//         data: {
+//           fill: charcoal,
+//           stroke: "transparent",
+//           strokeWidth: 0
+//         },
+//         labels: baseLabelStyles
+//       }
+//     },
+//     baseProps
+//   ),
+//   stack: assign(
+//     {
+//       colorScale: colors
+//     },
+//     baseProps
+//   ),
+//   tooltip: {
+//     style: assign({}, baseLabelStyles, { padding: 0, pointerEvents: "none" }),
+//     flyoutStyle: {
+//       stroke: charcoal,
+//       strokeWidth: 1,
+//       fill: "#f0f0f0",
+//       pointerEvents: "none"
+//     },
+//     flyoutPadding: 5,
+//     cornerRadius: 5,
+//     pointerLength: 10
+//   },
+//   voronoi: assign(
+//     {
+//       style: {
+//         data: {
+//           fill: "transparent",
+//           stroke: "transparent",
+//           strokeWidth: 0
+//         },
+//         labels: assign({}, baseLabelStyles, { padding: 5, pointerEvents: "none" }),
+//         flyout: {
+//           stroke: charcoal,
+//           strokeWidth: 1,
+//           fill: "#f0f0f0",
+//           pointerEvents: "none"
+//         }
+//       }
+//     },
+//     baseProps
+//   )
+// };
+
 export default class index extends Component {
   constructor(props) {
     super(props)
@@ -146,7 +455,43 @@ export default class index extends Component {
               </select>
             </div>
             <div className='graph-card-body'>
-
+            <VictoryChart
+              // domainPadding will add space to each side of VictoryBar to
+              // prevent it from overlapping the axis
+              domainPadding={10}
+              height={221}
+              width={550}
+              theme={VictoryTheme.material}
+            >
+              {/* <VictoryAxis dependentAxis
+                domain={[-10, 15]}
+                offsetX={50}
+                orientation="left"
+                standalone={false}
+                
+              /> */}
+              <VictoryAxis
+                // tickValues specifies both the number of ticks and where
+                // they are placed on the axis
+                tickValues={tickValues}
+                tickFormat={quaters}
+                style={{ }}
+              />
+              <VictoryAxis
+                dependentAxis
+                // tickFormat specifies how ticks should be displayed
+                tickValues={[0, 25, 50, 75, 100]}
+                tickFormat={(x) => x}
+              />
+              <VictoryBar
+                style={{ data: { fill: "#AADBEF" } }}
+                cornerRadius={{ top: 4, bottom: 4 }}
+                data={data}
+                width={33}
+                x="quarter"
+                y="earnings"
+              />
+            </VictoryChart>
             </div>
           </div>
         </div>
